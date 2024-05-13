10918217340ca42124920c84739f983fc701a64f
Syam Chaidayatullah <syamsamsungj36@gmail.com>
2024-05-13 22:21:52 +0700

    Revert "merge"
    This reverts commit e6d3926e4ef2bc95a774c2419bc3d87a0591ce60.

---
+++ Interface/Dashboard.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace project_pbo.Interface
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.sumberBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.contentPnl = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.sumberBtn);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(279, 894);
+            this.panel1.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(0, 418);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(279, 83);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Pengeluaran";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(0, 335);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(279, 83);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Pemasukan";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // sumberBtn
+            // 
+            this.sumberBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.sumberBtn.FlatAppearance.BorderSize = 0;
+            this.sumberBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sumberBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumberBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sumberBtn.Location = new System.Drawing.Point(0, 252);
+            this.sumberBtn.Name = "sumberBtn";
+            this.sumberBtn.Size = new System.Drawing.Size(279, 83);
+            this.sumberBtn.TabIndex = 2;
+            this.sumberBtn.Text = "Sumber Dana";
+            this.sumberBtn.UseVisualStyleBackColor = true;
+            this.sumberBtn.Click += new System.EventHandler(this.sumberBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(0, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 83);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dashboard";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(279, 169);
+            this.panel2.TabIndex = 0;
+            // 
+            // contentPnl
+            // 
+            this.contentPnl.Dock = System.Windows.Forms.DockStyle.Right;
+            this.contentPnl.Location = new System.Drawing.Point(413, 0);
+            this.contentPnl.Name = "contentPnl";
+            this.contentPnl.Size = new System.Drawing.Size(1243, 894);
+            this.contentPnl.TabIndex = 2;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1656, 894);
+            this.Controls.Add(this.contentPnl);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel contentPnl;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button sumberBtn;
+        private System.Windows.Forms.Button button4;
+    }
+}
