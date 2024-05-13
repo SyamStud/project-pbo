e6d3926e4ef2bc95a774c2419bc3d87a0591ce60
Syam Chaidayatullah <syamsamsungj36@gmail.com>
2024-05-13 22:17:50 +0700

    merge

---
+++ Interface/login.cs
@@ -0,0 +1,79 @@
+﻿using project_pbo.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_pbo.Interface
+{
+    public partial class login : Form
+       
+    {
+        user user = new user();
+        public login()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pass_txt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (user.CekLogin(username_txt.Text, pass_txt.Text))
+            {
+
+                Form1 form1 = new Form1();
+                form1.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("user id atau password salah", "Kesalahan",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                //user.SelectAll();
+                //user.Focus();
+
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
